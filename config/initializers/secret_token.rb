665acbc947a62475247ed5f454bd1f66bd1791ea
FlackAP <flack.ap@gmail.com>
2013-12-18 16:21:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flackbook::Application.config.secret_key_base = '4f80d103848a820eba9616841d513d9a009669d476f7a3b30267859b7ae78467450c431eba6f8a405a0cc11938b7cbe583aabece2bc1508c08c1a092f3872bb2'
